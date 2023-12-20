3af53895eb1ff542161d5ef0dce02a9726b2bb1a
Morabet <ismailelmorabet2@gmail.com>
2023-12-20 10:05:40 -0800

    states

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.city import City
 
 import os
 
@@ -30,7 +31,7 @@ class State(BaseModel, Base):
         def cities(self):
             from models import storage
 
-            dict_cities = storage.all("City").values()
+            dict_cities = list(storage.all(City).values())
             cities = []
 
             for obj in dict_cities:
