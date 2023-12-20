616d429606c023108e563e384ccd6487d62d7599
Morabet <ismailelmorabet2@gmail.com>
2023-12-20 08:35:23 -0800

    task 3 4 5

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -13,6 +13,8 @@ CREATE DATABASE IF NOT EXISTS performance_schema;
 
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 
-GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT on `performance_schema`.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
