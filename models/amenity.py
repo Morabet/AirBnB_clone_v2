bf449fb536f2f0c8ae64b7c019b0efafb5fae783
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-12-20 12:25:23 -0800

    am

--- models/amenity.py
+++ models/amenity.py
@@ -9,9 +9,9 @@ from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
 
+    __tablename__ = 'amenities'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
 
-        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
 
         place_amenities = relationship("Place", secondary="place_amenity",
