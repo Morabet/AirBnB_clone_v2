adc7c53dcf80214f870c33708aafa736c16eb2ed
Morabet <ismailelmorabet2@gmail.com>
2023-12-20 10:17:43 -0800

    user, place

--- models/place.py
+++ models/place.py
@@ -1,50 +1,45 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel, Base
 import os
-
-from sqlalchemy import Table, Column, Integer, Float, ForeignKey, String
+from models.base_model import BaseModel, Base
+from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
+from models.amenity import Amenity
+from models.review import Review
 
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-    place_amenity = Table(
-            'place_amenity',
-            Base.metadata,
-            Column('place_id', String(60), ForeignKey('places.id'),
-                   primary_key=True, nullable=False),
-            Column('amenity_id', String(60), ForeignKey('amenities.id'),
-                   primary_key=True, nullable=False)
-            )
+place_amenity = Table(
+        "place_amenity", Base.metadata,
+        Column('place_id', String(60),
+               ForeignKey("places.id"),
+               primary_key=True),
+        Column('amenity_id', String(60),
+               ForeignKey("amenities.id"),
+               primary_key=True)
+        )
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = "places"
+    __tablename__ = "places"
 
-        city_id = Column(String(60),
-                         ForeignKey('cities.id'),
-                         nullable=False)
-        user_id = Column(String(60),
-                         ForeignKey('users.id'),
-                         nullable=False)
-
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
-        description = Column(String(128), nullable=True)
+        description = Column(String(1024))
         number_rooms = Column(Integer, default=0, nullable=False)
         number_bathrooms = Column(Integer, default=0, nullable=False)
         max_guest = Column(Integer, default=0, nullable=False)
         price_by_night = Column(Integer, default=0, nullable=False)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-
-        reviews = relationship("Review", backref="place",
+        latitude = Column(Float)
+        longitude = Column(Float)
+        reviews = relationship('Review',
+                               backref="place",
                                cascade="all, delete-orphan")
-
-        amenities = relationship("Amenity", secondary="place_amenity",
+        amenities = relationship('Amenity',
+                                 secondary="place_amenity",
                                  viewonly=False)
-
     else:
         city_id = ""
         user_id = ""
@@ -58,38 +53,32 @@ class Place(BaseModel, Base):
         longitude = 0.0
         amenity_ids = []
 
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
-
-    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+    if os.getenv("HBNB_TYPE_STORAGE", None) != "db":
         @property
         def reviews(self):
-            """attribute that returns list of Review instances"""
-            reviews = models.storage.all("Review").values()
-            list_review = []
+            """Getter for the Review """
+            from models import storage
+            review_list = []
+            review_dict = stroge.all(Review).values()
+            for obj in review_dict:
+                if obj.place_id == self.id:
+                    review_list.append(obj)
 
-            for review in reviews:
-                if review.place_id == self.id:
-                    list_review.append(review)
-
-            return list_review
+            return review_list
 
         @property
         def amenities(self):
-            """attribute that returns list of Amenty instances"""
-            amenities = models.storage.all("Amenity").values()
-            list_amenities = []
-
-            for ame in amenities:
-
+            """Getter for the Amenity """
+            from models import storage
+            amenities_list = []
+            amenities_dict = stroge.all(Amenity).values()
+            for ame in amenities_dict:
                 if ame.id in self.amenity_ids:
-                    list_amenities.append(ame)
+                    amenities_list.append(ame)
 
-            return list_amenities
+            return amenities_list
 
         @amenities.setter
         def amenities(self, value):
-            """add 'Amenity.id' to 'amenitiy_id' """
             if type(value).__name__ == "Amenity":
                 self.amenity_ids.append(value.id)
--- models/user.py
+++ models/user.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-import os
 
-from sqlalchemy import Column, ForeignKey, String
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = "users"
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = "users"
-
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-
-        places = relationship("Place", backref="user",
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship('Place',
+                              backref="user",
                               cascade="all, delete-orphan")
-        reviews = relationship("Review", backref="user",
+        reviews = relationship('Review',
+                               backref="user",
                                cascade="all, delete-orphan")
-
     else:
         email = ''
         password = ''
@@ -30,6 +29,4 @@ class User(BaseModel, Base):
         last_name = ''
 
     def __init__(self, *args, **kwargs):
-
-        """initializes user"""
         super().__init__(*args, **kwargs)
