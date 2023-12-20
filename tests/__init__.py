bfd5649366ec4ce5496ef07afb3d2bec58efcfe5
Morabet <ismailelmorabet2@gmail.com>
2023-12-20 12:44:08 -0800

    tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Tests for the AirBnb clone modules.
+
+"""
+Tests for the AirBnb clone modules.
 """
 import os
 from typing import TextIO
@@ -7,10 +9,8 @@ from models.engine.file_storage import FileStorage
 
 
 def clear_stream(stream: TextIO):
-    """Clears the contents of a given stream
-
-    Args:
-        stream (TextIO): The stream to clear.
+    """
+    Clears the contents of a given stream
     """
     if stream.seekable():
         stream.seek(0)
@@ -18,19 +18,16 @@ def clear_stream(stream: TextIO):
 
 
 def delete_file(file_path: str):
-    """Removes a file if it exists.
-    Args:
-        file_path (str): The name of the file to remove.
+    """
+    Removes a file if it exists.
     """
     if os.path.isfile(file_path):
         os.unlink(file_path)
 
 
 def reset_store(store: FileStorage, file_path='file.json'):
-    """Resets the items in the given store.
-    Args:
-        store (FileStorage): The FileStorage to reset.
-        file_path (str): The path to the store's file.
+    """
+    Resets the items in the given store.
     """
     with open(file_path, mode='w') as file:
         file.write('{}')
@@ -39,13 +36,8 @@ def reset_store(store: FileStorage, file_path='file.json'):
 
 
 def read_text_file(file_name):
-    """Reads the contents of a given file.
-
-    Args:
-        file_name (str): The name of the file to read.
-
-    Returns:
-        str: The contents of the file if it exists.
+    """
+    Reads the contents of a given file.
     """
     lines = []
     if os.path.isfile(file_name):
@@ -56,11 +48,8 @@ def read_text_file(file_name):
 
 
 def write_text_file(file_name, text):
-    """Writes a text to a given file.
-
-    Args:
-        file_name (str): The name of the file to write to.
-        text (str): The content of the file.
+    """
+    Writes a text to a given file.
     """
     with open(file_name, mode='w') as file:
         file.write(text)
